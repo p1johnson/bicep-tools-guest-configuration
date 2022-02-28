5edd0a26f30385dca623475657ef7be5921945ec
Paul Johnson <paul.johnson@contentandcloud.com>
2022-02-28 20:41:10 +0000

    Update Modules.ps1

--- scripts/Modules.ps1
+++ scripts/Modules.ps1
@@ -1,2 +1,2 @@
-Install-Module -Name PSDesiredStateConfiguration -Scope AllUsers -Force
+Install-Module -Name PSDSCResources -Scope AllUsers -Force
 Install-Module -Name GuestConfiguration -Scope AllUsers -Force
