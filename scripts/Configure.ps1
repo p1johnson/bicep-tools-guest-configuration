1ba09858d8e8d72d54d18d0101edcb512a474599
Paul Johnson <paul.johnson@contentandcloud.com>
2022-03-01 09:42:59 +0000

    Update Configure.ps1

--- scripts/Configure.ps1
+++ scripts/Configure.ps1
@@ -1,5 +1,6 @@
 if (!(Test-Path -Path $env:ProgramFiles\PowerShell\7 -PathType Container)) {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
     Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.1.3/PowerShell-7.1.3-win-x64.msi" -UseBasicParsing -OutFile "PowerShell-7.1.3-win-x64.msi"
-    Start-Process -Wait -FilePath 'msiexec.exe' -ArgumentList '/i "PowerShell-7.1.3-win-x64.msi" /quiet /l* "PowerShell-7.1.3-win-x64.log"'
+    #Start-Process -Wait -FilePath 'msiexec.exe' -ArgumentList '/i "PowerShell-7.1.3-win-x64.msi" /quiet /l* "PowerShell-7.1.3-win-x64.log"'
 }
-Invoke-Expression -Command "& '$env:ProgramFiles\PowerShell\7\pwsh.exe' -File Modules.ps1"
+#Invoke-Expression -Command "& '$env:ProgramFiles\PowerShell\7\pwsh.exe' -File Modules.ps1"
